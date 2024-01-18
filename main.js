8147229a558a01fed4cc3750681dff6e124732fa
telinv <linatorgashova2002@mail.ru>
2024-01-18 14:32:08 +0300

    upd styles

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ document.querySelector('.card_main').style.display = 'none';
 import "./src/scripts/cityApi/cityCurrency.js"
 //console.log('test-check:');
 import "./src/scripts/cityApi/apiWeather.js"
-//import "./src/scripts/cityApi/usefulInfo.js"
+import "./src/scripts/cityApi/usefulInfo.js"
+import "./src/scripts/cityApi/region.js"
 
