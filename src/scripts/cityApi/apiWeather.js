8e875a4b8946e9f33c71125ba00709cef7948b8c
EvgeniyaKalinichenko <137879077+EvgeniyaKalinichenko@users.noreply.github.com>
2024-01-18 16:23:47 -0600

    Add current date at page and some styles

--- src/scripts/cityApi/apiWeather.js
+++ src/scripts/cityApi/apiWeather.js
@@ -25,3 +25,37 @@ searchInput.addEventListener("keydown", (event) => {
         //searchInput.value = "";
     }
     });
+
+    const currentDate = document.querySelector('.currentDate');
+
+    let Data = new Date();
+    let year = Data.getFullYear();
+    let month = Data.getMonth();
+    let day = Data.getDate();
+    let hours = zero_first_format(Data.getHours());
+    let minutes = zero_first_format(Data.getMinutes());
+    
+    function zero_first_format(value) {
+        if (value < 10)
+        {
+            value='0'+value;
+        }
+        return value;
+    }
+
+    switch (month) {
+        case 0: month="Jan."; break;
+        case 1: month="Feb."; break;
+        case 2: month="Mar."; break;
+        case 3: month="Apr."; break;
+        case 4: month="May"; break;
+        case 5: month="June"; break;
+        case 6: month="July"; break;
+        case 7: month="Aug."; break;
+        case 8: month="Sept."; break;
+        case 9: month="Oct."; break;
+        case 10: month="Nov."; break;
+        case 11: month="Dec."; break;
+    }
+    
+    currentDate.innerHTML = "Today is " + day + " " + month + " " + year + "   " + hours + ':' + minutes;
