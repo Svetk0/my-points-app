6467fb8892e9447009f992cfd206bf9aacb06992
svetk0 <sv.solomatnikova@gmail.com>
2024-01-19 21:49:06 +0300

    styles updates, add errors

--- src/scripts/cityApi/apiCityMainInfo.js
+++ src/scripts/cityApi/apiCityMainInfo.js
@@ -7,10 +7,12 @@ const cityName = document.querySelector('.card-name');
 const cityCurrency = document.querySelector('.card-currency');
 const countryName = document.querySelector('.card-info');
 const mapLocation = document.querySelector('.card-timezone-link');
+const containerErrors = document.querySelector('.card-main-error');
 // const formContainer = document.querySelector('.form_container');
 // const countryInfoContainer = document.querySelector('.countryInfoContainer');
-
+containerErrors.style.display = 'none';
 submitBtn.addEventListener('click', enteredCity);
+
 
 
 export default function enteredCity() {
@@ -18,6 +20,7 @@ export default function enteredCity() {
     document.querySelector('.card_main').style.display = 'flex';
     console.log('City input: ' + autoCompleteInput.value);
     getCountryData(autoCompleteInput.value);
+    containerErrors.style.display = 'none';
     //printCityName(autoCompleteInput.value);
     ////document.querySelector('#input').value = '';
 }
@@ -35,26 +38,30 @@ function printCityName(city) {
 }
 function printLocation(location) {
     mapLocation.href = location;
-    console.log('location: '+location);
+    console.log('location: ' + location);
 }
 async function getCountryData(capitalCity) {
-    const countryResource = await fetch('https://restcountries.com/v3.1/capital/' + capitalCity);
-    const data = await countryResource.json()
-    console.log(data);
+    try {
+        const countryResource = await fetch('https://restcountries.com/v3.1/capital/' + capitalCity);
+        const data = await countryResource.json()
+        console.log(data);
 
-    //currency output
-    printCurrency((data[0].currencies[Object.keys(data[0].currencies)[0]].name) + ' (' + (data[0].currencies[Object.keys(data[0].currencies)[0]].symbol) + ')');
-    //name of city output
-    printCityName(data[0].capital);
-    //name of country output
-    printCountryName((data[0].name.common));
-    //a link in maps
-    printLocation((data[0].maps.googleMaps));
+        //Print Info
+        printCurrency((data[0].currencies[Object.keys(data[0].currencies)[0]].name) + ' (' + (data[0].currencies[Object.keys(data[0].currencies)[0]].symbol) + ')');
+        
+        printCityName(data[0].capital);
+      
+        printCountryName((data[0].name.common));
+        
+        //a link in maps
+        printLocation((data[0].maps.googleMaps));
 
-    countryNames = data.map((country) => {
-        return country.name.common;
-    })
-
-
+       
+    }
+    catch (error) { 
+        containerErrors.style.display = 'flex';
+        console.error('Ошибка в поиске города:', error);
+        containerErrors.textContent = 'Ошибка в поиске города: \n' +error;
+    }
 }
 
