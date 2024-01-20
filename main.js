ef8f42d30232a7e3b8b4a1542f20c548b2690a5e
svetk0 <sv.solomatnikova@gmail.com>
2024-01-20 21:32:38 +0300

    fix bugs

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 document.querySelector('.card_main').style.display = 'none';
-import "./src/scripts/cityApi/apiCityMainInfo.js"
+import "./src/scripts/cityApi/apiCityMainInfo.js";
 //console.log('test-check:');
-import "./src/scripts/cityApi/apiWeather.js"
-import "./src/scripts/cityApi/usefulInfo.js"
-import "./src/scripts/cityApi/region.js"
+import "./src/scripts/cityApi/apiWeather.js";
+import "./src/scripts/cityApi/usefulInfo.js";
+import "./src/scripts/cityApi/region.js";
+
 
