604de066f1456b33b9c08f406f119ac9733b9afc
svetk0 <sv.solomatnikova@gmail.com>
2024-01-30 00:39:46 +0300

    fix bugs

--- src/scripts/cityApi/catchErrors.js
+++ src/scripts/cityApi/catchErrors.js
@@ -1,35 +1,37 @@
 let errors = [];
 
 function checkValidity(input) {
-    let validity = input.validity;
+    //let validity = input.validity;
     const errors = [];
-    if (validity.patternMismatch) {
-        errors.push("Неверный формат заполнения");
+    if (input) {
+        errors.push("Error in get request");
     }
+    // if (validity.patternMismatch) {
+    //     errors.push("Неверный формат заполнения");
+    // }
+    // if (validity.rangeOverflow) {
+    //     errors.push("Значение превосходит максимально допустимое");
+    // }
   
-    if (validity.rangeOverflow) {
-        errors.push("Значение превосходит максимально допустимое");
-    }
+    // if (validity.rangeUnderflow) {
+    //     errors.push("Значение меньше минимально допустимого");
+    // }
   
-    if (validity.rangeUnderflow) {
-        errors.push("Значение меньше минимально допустимого");
-    }
+    // if (validity.stepMismatch) {
+    //     errors.push("Недопустимое значение в соответствии с шагом");
+    // }
   
-    if (validity.stepMismatch) {
-        errors.push("Недопустимое значение в соответствии с шагом");
-    }
+    // if (validity.tooLong) {
+    //     errors.push("Значение слишком длинное");
+    // }
   
-    if (validity.tooLong) {
-        errors.push("Значение слишком длинное");
-    }
+    // if (validity.tooShort) {
+    //     errors.push("Значение слишком короткое");
+    // }
   
-    if (validity.tooShort) {
-        errors.push("Значение слишком короткое");
-    }
-  
-    if (validity.valueMissing) {
-        errors.push("Необходимо заполнить поле");
-    }
+    // if (validity.valueMissing) {
+    //     errors.push("Необходимо заполнить поле");
+    // }
     return errors.length ? errors.join(". \n") : "";
 }
 
@@ -38,13 +40,14 @@ export function checkAll(inputs) {
     //let inputs = document.querySelectorAll("input");
     let errorDiv = document.querySelector(".card-main-error");
     errorDiv.textContent = "";
+    errors.push(checkValidity(inputs));
 
-    for (let input of inputs) {
-        let error = checkValidity(input);
-        if (error) {
-            errors.push(error);
-        }
-    }
+    // for (let input of inputs) {
+    //     let error = checkValidity(input);
+    //     if (error) {
+    //         errors.push(error);
+    //     }
+    // }
 
     if (errors.length > 0) {
         errorDiv.classList.remove('errorsInfo-content');
